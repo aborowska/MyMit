b1cb2f2cb5917b6f8647f5dcb37107311f165971
aborowska <agnborowska@gmail.com>
2016-06-23 17:48:05 +0200

    GAS update

--- QERMit_t_gas.m
+++ QERMit_t_gas.m
@@ -44,6 +44,9 @@ ES_prelim = zeros(N_sim,1);
 accept = zeros(N_sim,1);
 time_prelim = zeros(2,1);
 
+VaR_pmit = zeros(N_sim,1);
+ES_pmit = zeros(N_sim,1);
+time_pmit = zeros(2,1);
 
 % theta = [mu, omega, A, B, nu]
 mu_init = [0, 0.01, 0.1, 0.89, 8];
@@ -135,6 +138,7 @@ w_hl = lnk_hl - lnd_hl;
 w_hl = exp(w_hl - max(w_hl));
 
 %% PMitISEM
+algo = 'PMitISEM';
 partition = [1,DD+2:H+DD];
 d = H+DD;
 
@@ -168,4 +172,59 @@ time_pmit(1,1) = time_pmit(1,1) + toc;
 if save_on
     name = ['results/PMitISEM/',model,'_',algo,'_',num2str(p_bar),'_H',num2str(H),'_VaR_results_Nsim',num2str(N_sim),'.mat'];
     save(name,'pmit','CV_mix','CV','iter')
+end
+
+
+
+%% VaR with PMit
+tic
+for sim = 2:N_sim   
+    fprintf('\nVaR IS iter: %d\n',sim)
+
+    theta1 = rmvgt2(M/2, mit1.mu, mit1.Sigma, mit1.df, mit1.p); 
+    eps1 = zeros(M/2, H);
+    for hh = 1:H
+        eps1(:,hh) = trnd(theta1(:,DD)); % ERRORS ARE iid T!!
+    end
+    draw1 = [theta1, eps1];
+    clear theta1 eps1
+
+    draw_pmit  = fn_p_rmvgt(M/2, pmit, d, partition, [], fn_const_X);  
+    draw_opt = [draw1; draw_pmit];
+    clear draw1 %draw_pmit
+
+    kernel = @(xx) posterior_t_gas_hyper_mex(xx, y, hyper, GamMat);
+    lnk_opt = kernel(draw_opt(:,1:5)); 
+
+%     eps_pdf = zeros(M, 1);
+%     for hh = 1:H
+%         eps_pdf = eps_pdf + log(tpdf(draw_opt(:,DD+hh),draw_opt(:,DD)));
+%     end
+    eps_pdf = duvt(draw_opt(:,DD+1:H+DD), draw_opt(:,DD), H, true);
+    lnk_opt = lnk_opt + eps_pdf;
+
+    % optimal weights
+    exp_lnd1 = 0.5*exp(eps_pdf + dmvgt(draw_opt(:,1:DD), mit1, true, GamMat));
+    exp_lnd2 = fn_dpmit(draw_opt, pmit, partition, fn_const_X, true, GamMat);
+
+    exp_lnd2 = 0.5*exp(exp_lnd2);
+    exp_lnd = exp_lnd1 + exp_lnd2;
+    lnd_opt = log(exp_lnd);
+    w_opt = fn_ISwgts(lnk_opt, lnd_opt, false);
+
+    % IS VaR estimation
+    f_T = volatility_t_gas_mex(draw_opt(:,1:DD), y);
+    [y_opt, ~] = predict_t_gas(draw_opt(:,1:DD), y_T, f_T, H, draw_opt(:,DD+1:H+DD));
+    dens = struct('y',y_opt,'w',w_opt,'p_bar',p_bar);
+    IS_estim = fn_PL(dens, 1);
+    VaR_pmit(sim,1) = IS_estim(1,1);
+    ES_pmit(sim,1) = IS_estim(1,2);   
+
+    fprintf('IS 100*%4.2f%% VaR estimate: %6.4f (%s, %s). \n', p_bar, VaR_pmit(sim,1), model, algo);  
+	
+time_pmit(2,1) = toc/N_sim;
+
+if save_on
+    name = ['results/PMitISEM/',model,'_',algo,'_',num2str(p_bar),'_H',num2str(H),'_VaR_results_Nsim',num2str(N_sim),'.mat'];
+    save(name,'pmit','CV_mix','CV','iter','VaR_pmit','ES_pmit','time_pmit')
 end
